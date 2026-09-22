3ad577b49b98691cce245989f176e749571f108d
alphadev254-droid <alphadev254@gmail.com>
2026-09-22 18:40:38 +0300

    Disable marketer withdrawal endpoints

--- src/routes/referrals.ts
+++ src/routes/referrals.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 import {
-  confirmWithdrawal,
   getMyReferrerDashboard,
   getMyPayoutOptions,
   listAdminReferrers,
   registerReferrer,
-  requestWithdrawalOtp,
   updateMyPayoutSetup,
   updateAdminReferrerStatus,
 } from '../controllers/referralController';
@@ -17,9 +15,8 @@ router.post('/register', registerReferrer);
 router.get('/me', authenticate, authorize('referrer'), getMyReferrerDashboard);
 router.get('/payout-options', authenticate, authorize('referrer'), getMyPayoutOptions);
 router.put('/payout-setup', authenticate, authorize('referrer'), updateMyPayoutSetup);
-router.post('/withdrawals/otp', authenticate, authorize('referrer'), requestWithdrawalOtp);
-router.post('/withdrawals', authenticate, authorize('referrer'), confirmWithdrawal);
 router.get('/admin/referrers', authenticate, authorize('ministry_admin', 'system_admin'), listAdminReferrers);
 router.patch('/admin/referrers/:id/status', authenticate, authorize('ministry_admin', 'system_admin'), updateAdminReferrerStatus);
 
 export default router;
+
